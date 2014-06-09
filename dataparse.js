50d71692c433fbb088dd8b691b0a2b7d71babffe
Jan Jaap Meijerink <janjaap.meijerink@hotmail.com>
2014-06-09 16:27:07 +0200

    asdf

--- dataparse.js
+++ dataparse.js
@@ -4,6 +4,3 @@
 		d3.csv("browser-ww-monthly-201305-201405.csv", function (d) {
 		data = d;
 		});
-		for (i=0;i<5;i++) {
-			marketShares.push(data[i]["Market Share Perc. (May 2013 to May 2014)"]);
-			websites.push(data[i].Browser);}
