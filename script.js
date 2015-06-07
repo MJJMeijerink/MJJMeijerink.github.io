e08cb61019716303bdb26af7ce6de9a54bf7f922
Jan Jaap Meijerink <janjaap.meijerink@hotmail.com>
2015-06-07 18:22:30 +0200

    test
    test

---
+++ script.js
@@ -0,0 +1,29 @@
+	var clicked;
+	var prev;
+	var prev2;
+	function infoShow(x) {
+		document.getElementById('Welcome').style.display = "none";
+		document.getElementById('WelcomeID').style.fontWeight = "initial";
+	  	var y = document.getElementById(x);
+		y.style.display  = "initial";
+		var z = document.getElementById(x+"ID");
+		z.style.fontWeight = "bold";
+		if (prev && prev != y) {
+			prev.style.display = "none";
+			prev2.style.fontWeight="initial";
+			}
+		prev = y;
+		prev2 = z;
+		return;
+	  }
+	
+	function mouseIn(x) {
+			x.style.fontWeight = "bold";
+	}
+
+	function mouseOut(x) {
+		if (typeof prev2 === 'undefined')
+			{prev2 = document.getElementById('WelcomeID');}
+		else if (prev2 != x)
+			{x.style.fontWeight = "initial";}
+	}
